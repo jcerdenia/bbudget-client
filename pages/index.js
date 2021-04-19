c4fa65ac36576d58355f49c497bd749184280247
Joshua Cerdenia <joshcerdenia@gmail.com>
2021-04-19 20:52:07 +0800

    working on the register function

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,11 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import View from '../components/View';
-import LoginForm from '../components/LoginForm'
 
 export default function Home() {
   return (
     <View title="Log In">
-      <LoginForm />
+      
     </View>
   );
 }
---
+++ pages/register.js
@@ -0,0 +1,108 @@
+import { useState } from 'react'
+import { Form, Button } from 'react-bootstrap'
+import Router from 'next/router'
+import Swal from 'sweetalert2'
+
+export default function Register() {
+	const [firstName, setFirstName] = useState("")
+	const [lastName, setLastName] = useState("")
+	const [email, setEmail] = useState("")
+	const [mobileNo, setMobileNo] = useState(0)
+	const [password1, setPassword1] = useState("")
+	const [password2, setPassword2] = useState("")
+
+	const isActive = useState(true)
+
+	function registerUser(e) {
+		e.preventDefault()
+		fetch('https://aqueous-atoll-99638.herokuapp.com/api/users/email-exists', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ email: email })
+		})
+		.then((res) => res.json())
+		.then((data) => {
+			if (data === false) {
+				// Allow registration
+				fetch('https://aqueous-atoll-99638.herokuapp.com/api/users/register', {
+					method: 'POST',
+					headers: { 'Content-Type': 'application/json' },
+					body: JSON.stringify({
+						firstName: firstName,
+						lastName: lastName,
+						email: email,
+						mobileNo: mobileNo,
+						password: password1
+					})
+				})
+				.then((res) => res.json())
+				.then((data) => {
+					if (data === true) {
+						Swal.fire({
+							icon: "success",
+							title: "Successfully registered",
+							text: "Thank you for registering."
+						})
+						// Redirect to login page
+						Router.push('/')
+					} else {
+						Swal.fire({
+							icon: "error",
+							title: "Registration failed",
+							text: "Something went wrong."
+						})
+					}
+				})
+			} else {
+				Swal.fire({
+					icon: 'error',
+					title: 'Registration failed',
+					text: 'That email address is already in use.'
+				})
+			}
+		})
+	}
+
+   return (
+		<>
+			<h1 className="mt-5 pt-5 text-center">Register</h1>
+			<Form onSubmit={(e) => registerUser(e)} className="mb-3">
+				<Form.Group controlId="userFirstName">
+					<Form.Label>First Name</Form.Label>
+					<Form.Control type="text" placeholder="Enter First Name" value={firstName} onChange={e => setFirstName(e.target.value)} required/>
+				</Form.Group>
+
+				<Form.Group controlId="userLastName">
+					<Form.Label>Last Name</Form.Label>
+					<Form.Control type="text" placeholder="Enter Last Name" value={lastName} onChange={e => setLastName(e.target.value)} required/>
+				</Form.Group>
+
+				<Form.Group controlId="userEmail">
+					<Form.Label>Email</Form.Label>
+					<Form.Control type="email" placeholder="Enter Email" value={email} onChange={e => setEmail(e.target.value)} required/>
+				</Form.Group>
+
+				<Form.Group controlId="mobileNo">
+					<Form.Label>Mobile Number</Form.Label>
+					<Form.Control type="number" placeholder="Enter Mobile No." value={mobileNo} onChange={e => setMobileNo(e.target.value)} required/>
+				</Form.Group>
+
+				<Form.Group controlId="password1">
+					<Form.Label>Password</Form.Label>
+					<Form.Control type="password" placeholder="Enter Password" value={password1} onChange={e => setPassword1(e.target.value)} required/>
+				</Form.Group>
+
+				<Form.Group controlId="password2">
+					<Form.Label>Confirm Password</Form.Label>
+					<Form.Control type="password" placeholder="Confirm Password" value={password2} onChange={e => setPassword2(e.target.value)} required/>
+				</Form.Group>
+
+				
+				<Button variant="primary" type="submit">Register</Button>					
+				
+				{/*<Button variant="primary" disabled>Register</Button>*/}
+				
+			</Form>
+		</>
+		) 
+}
