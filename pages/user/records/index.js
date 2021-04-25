38218fced121e5f9991fa9df1232e04bda508f7b
Joshua Cerdenia <joshcerdenia@gmail.com>
2021-04-25 22:27:20 +0800

    fix error

--- pages/user/records/index.js
+++ pages/user/records/index.js
@@ -1,6 +1,5 @@
 import { Fragment, useState, useEffect } from 'react'
 import { Table, Button, Row, Col, InputGroup, FormControl, Form, Card } from 'react-bootstrap'
-import Link from 'next/link'
 import View from '../../../components/View'
 import RecordsView from '../../../components/RecordsView'
 import CategoriesView from '../../../components/CategoriesView'
